1160962b6f35abf35498eaf8907c744e1df7ed74
TorresHub9710 <heortiz34510@universidadean.edu.co>
2022-11-12 09:46:29 -0500

    Cambios búsqueda

--- JS/formulario.js
+++ JS/formulario.js
@@ -1,3 +1,15 @@
+// Función Menú //
+
+document.getElementById("icono-menu").addEventListener("click",mostrar_menu);
+
+function mostrar_menu() {
+
+    document.getElementById("move-contenido").classList.toggle('move-contenedor-principal');
+    document.getElementById("mostrar-menu").classList.toggle('mostrar-menu-lateral');
+}
+
+
+// Variables - Formualrio //
 function captura(){
     var nombreReceta, tiempoReceta, listaIngredientes, instrucciones, videos, imagen;
     nombreReceta=document.getElementById("recetaName").value;
