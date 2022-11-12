99688a966ee310a2f5377e9cdb34f86789771111
PaolaRo28 <115380177+PaolaRo28@users.noreply.github.com>
2022-11-12 15:05:47 -0500

    eliminación_boton

--- JS/principal.js
+++ JS/principal.js
@@ -2,65 +2,57 @@ const menuHam = document.querySelector("#menu-ham"),
   menuLinks= document.querySelector("#menu-links")
 
 
-  menuHam.addEventListener('click',(e)=>{
+  menuHam.addEventListener("click",(e)=>{
   menuLinks.classList.toggle("links_visible");
   });
 
 
 
 
-const slides = document.querySelectorAll('.slide');
-const next = document.querySelector('#next');
+const slides = document.querySelectorAll(".slide");
+const next = document.querySelector("#next");
 const prev = document.querySelector('#prev');
-const intervalTime = 5000;
-let slideInterval;
+
+
 
 const nextSlide = () => {
   // Get current class
-  const current = document.querySelector('.current');
+  const current = document.querySelector(".current");
   // Remove current class
-  current.classList.remove('current');
+  current.classList.remove("current");
   // Check for next slide
   if (current.nextElementSibling) {
     // Add current to next sibling
-    current.nextElementSibling.classList.add('current');
+    current.nextElementSibling.classList.add("current");
   } else {
     // Add current to start
-    slides[0].classList.add('current');
+    slides[0].classList.add("current");
   }
-  setTimeout(() => current.classList.remove('current'));
+
 };
 
 const prevSlide = () => {
   // Get current class
-  const current = document.querySelector('.current');
+  const current = document.querySelector(".current");
   // Remove current class
-  current.classList.remove('current');
+  current.classList.remove("current");
   // Check for prev slide
   if (current.previousElementSibling) {
     // Add current to prev sibling
-    current.previousElementSibling.classList.add('current');
+    current.previousElementSibling.classList.add("current");
   } else {
     // Add current to last
-    slides[slides.length - 1].classList.add('current');
+    slides[slides.length - 1].classList.add("current");
   }
-  setTimeout(() => current.classList.remove('current'));
+
 };
 
 // Button events
-next.addEventListener('click', e => {
+next.addEventListener("click", e => {
   nextSlide();
-  if (auto) {
-    clearInterval(slideInterval);
-    slideInterval = setInterval(nextSlide, intervalTime);
-  }
 });
 
-prev.addEventListener('click', e => {
+prev.addEventListener("click", e => {
   prevSlide();
-  if (auto) {
-    clearInterval(slideInterval);
-    slideInterval = setInterval(nextSlide, intervalTime);
   }
-});
-
+);
